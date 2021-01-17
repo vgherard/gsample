aa7616fe7ee8f3af83f656abac5e9914b1e2323f
vgherard <vgherard@sissa.it>
2021-01-17 19:17:11 +0100

    Make API identical to base::sample()

--- R/gsample.R
+++ R/gsample.R
@@ -1,33 +1,27 @@
 #' Efficient weighted sampling without replacement
 #'
-#' Sample without replacement using the Gumbel-Max trick
+#' Weighted sampling without replacement using the Gumbel-Max trick
 #' (c.f. \url{https://arxiv.org/pdf/1903.06059.pdf}).
 #
 #' @author Valerio Gherardi
 #' @md
 #'
+#' @param x a vector of one or more elements from which to choose.
 #' @param n length one integer. The total number of categories to choose from;
 #' See ‘Details’.
 #' @param size length one integer. Size of sample.
 #' @param replace TRUE or FALSE. Sample with replacement? Defaults to
 #' \code{FALSE}.
-#' @param weights either \code{NULL}, or a numeric vector of length \code{n},
-#' containing the (unnormalized) probabilities, or the (unnormalized)
-#' log-probabilities for sampling categories
-#' (depending on the value of \code{log_weights} argument, see below).
+#' @param prob either \code{NULL}, or a numeric vector of length \code{n},
+#' containing probability weights for sampling the various classes.
 #' If \code{NULL} (default), sampling is performed assuming uniform
 #' probabilities.
-#' @param log_weights TRUE or FALSE. Should the values given in \code{weights}
-#' be interpreted as (unnormalized) log-probabilities? Defaults to \code{FALSE}.
-#' @return an integer vector if \code{map} is \code{NULL}, otherwise a vector
-#' of the same type as \code{map}.
+#' @return an integer vector of class indexes for \code{gsample.int}, a vector
+#' of the same type of \code{x} for \code{gsample}.
 #' @examples
 #' set.seed(840)
-#' size <- 5
-#' weights <- rnorm(length(letters), sd = 10)
-#' gsample(size = 5, weights, map = letters)
-#' @details
-#'
+#' gsample(letters, 5, prob = runif(length(letters)))
+#' gsample.int(10, 3, prob = 10:1)
 #' @name gsample
 #'
 
@@ -36,18 +30,15 @@
 gsample.int <- function(n,
 			size = n,
 			replace = FALSE,
-			weights = NULL,
-			log_weights = FALSE
+			prob = NULL
 			)
 {
-	argcheck(n, size, replace, weights, log_weights)
+	argcheck(n, size, replace, prob)
 
-	if (replace || is.null(weights)) {
-		if (log_weights)
-			prob <- exp(weights)
+	if (replace || is.null(prob)) {
 		index <- sample.int(n, size, replace, prob)
 	} else {
-		index <- gsample_wor(n, size, weights, log_weights)
+		index <- gsample_wor(n, size, prob)
 	}
 
 	return(index)
@@ -58,11 +49,17 @@ gsample.int <- function(n,
 gsample <- function(x,
 		    size,
 		    replace = FALSE,
-		    weights = NULL,
-		    log_weights = FALSE
+		    prob
 		    )
 {
-	if (!is.vector(x) && length(x) >= 1)
-		stop("'x' must be a vector of positive length.")
-	x[gsample.int(length(x), size, replace, weights, log_weights)]
+	if (length(x) == 1L && is.numeric(x) && is.finite(x) && x >= 1) {
+		if (missing(size))
+			size <- x
+		gsample.int(x, size, replace, prob)
+	} else {
+		if (missing(size))
+			size <- length(x)
+		x[gsample.int(length(x), size, replace, prob)]
+	}
+
 }
