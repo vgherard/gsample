15e4654dc32dc80b6a5cd0649f7da917c69c0e7f
vgherard <vgherard@sissa.it>
2021-01-17 19:17:46 +0100

    Remove unnecessary IndexScorePair struct

--- src/gsample.h
+++ src/gsample.h
@@ -3,17 +3,8 @@
 
 // [[Rcpp::plugins(cpp11)]]
 #include <Rcpp.h>
+#include <Rmath.h>
 #include <vector>
 #include <algorithm>
-#include <random>
-
-struct IndexScorePair {
-	int index;
-	double score;
-	IndexScorePair() : index(-1), score(0) {}
-	IndexScorePair(int i, double s) : index(i), score(s) {}
-	friend bool operator<(const IndexScorePair& l, const IndexScorePair& r)
-		{ return l.score < r.score; }
-};
 
 #endif // GSAMPLE_H
